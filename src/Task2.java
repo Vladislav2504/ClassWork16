2ace11e9895d5a7b55ebd6d9c29553c6f5e6ea11
DZ-2 <Fomin.vlad25@mail.ru>
2022-06-30 15:53:27 +0300

    Update

--- src/Task2.java
+++ src/Task2.java
@@ -6,29 +6,29 @@ import java.util.regex.Pattern;
 public class Task2 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String nameDoc = scanner.nextLine();
-
-        String regexDoc = "\\d[0-9]{4}-\\D[a-z]{3}-\\d[0-9]{4}-\\D[a-z]{3}-\\d[0-9]{1}\\D[a-z]{1}\\d[0-9]{1}-\\D[a-z]{1}";
-        String regexNum = "\\+375\\s\\D[(]\\[29|33|44|]{2}\\D[)]\\s\\d{3}-\\d{2}-\\d{2}";
-        String regexMail = "\\w\\D[@]\\D{1,9}.\\D{1,3}";
-
-        Pattern patternDoc = Pattern.compile(regexDoc);
-        Pattern patternNum = Pattern.compile(regexNum);
-        Pattern patternMail = Pattern.compile(regexMail);
-
-        try {FileReader file = new FileReader(nameDoc);
-            Scanner scanner1 = new Scanner(file);
-            while (scanner1.hasNextLine()){
-                String a = scanner1.nextLine();
-
-                if (a.endsWith("txt")){
-                    System.out.println();
-                }
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+//        Scanner scanner = new Scanner(System.in);
+//        String nameDoc = scanner.nextLine();
+//
+//        String regexDoc = "\\d[0-9]{4}-\\D[a-z]{3}-\\d[0-9]{4}-\\D[a-z]{3}-\\d[0-9]{1}\\D[a-z]{1}\\d[0-9]{1}-\\D[a-z]{1}";
+//        String regexNum = "\\+375\\s\\D[(]\\[29|33|44|]{2}\\D[)]\\s\\d{3}-\\d{2}-\\d{2}";
+//        String regexMail = "\\w\\D[@]\\D{1,9}.\\D{1,3}";
+//
+//        Pattern patternDoc = Pattern.compile(regexDoc);
+//        Pattern patternNum = Pattern.compile(regexNum);
+//        Pattern patternMail = Pattern.compile(regexMail);
+//
+//        try {FileReader file = new FileReader(nameDoc);
+//            Scanner scanner1 = new Scanner(file);
+//            while (scanner1.hasNextLine()){
+//                String a = scanner1.nextLine();
+//
+//                if (a.endsWith("txt")){
+//                    System.out.println();
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 }
